1b75474668348ca034826f782fe7718a4d873c7a
Zhen Jia Huang <zhjihuang@gmail.com>
2026-01-23 06:55:47 +0000

    Cleaned up install script and used BlueBuild where appropriate.

--- files/scripts/install-kernel-surface.sh
+++ files/scripts/install-kernel-surface.sh
@@ -1,37 +1,43 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 set -ouex pipefail
 
-# remove kernel locks
-dnf5 versionlock delete kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-lib,-headers,-devel,-devel-matched}
-
-# Add the Surface Linux repo
-dnf5 config-manager \
-    addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
-
-# Install the Surface Linux kernel and related packages
-dnf5 -y install --allowerasing kernel-surface iptsd libwacom-surface surface-secureboot surface-control
-
-# Remove the default Fedora kernel and related packages
-dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-
+KERNEL_LOCK="kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-lib,-headers,-devel,-devel-matched}"
 KERNEL_SUFFIX="surface"
-QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
-/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-
-# Prevent kernel stuff from upgrading again
-dnf5 versionlock add kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-lib,-headers,-devel,-devel-matched}
-
-SURFACE_PACKAGES=(
-    iptsd
-    libcamera
-    libcamera-tools
-    libcamera-gstreamer
-    libcamera-ipa
-    pipewire-plugin-libcamera
+REPOSITORY_SURFACE="https://pkg.surfacelinux.com/fedora/linux-surface.repo"
+PACKAGES_KERNEL_DEFAULT=(
+  kernel
+  kernel-core
+  kernel-modules
+  kernel-modules-core
+  kernel-modules-extra
+)
+PACKAGES_KERNEL_SURFACE=(
+  kernel-surface
+  iptsd
+  libwacom-surface
+  surface-secureboot
+  surface-control
 )
 
-dnf5 install --assumeyes --skip-unavailable "${SURFACE_PACKAGES[@]}"
+# remove kernel locks
+dnf5 versionlock delete ${KERNEL_LOCK} 
+
+# Add the Surface Linux repo
+dnf5 config-manager addrepo --from-repofile=${REPOSITORY_SURFACE}
+
+# Install the Surface Linux kernel and related packages
+dnf5 -y install --allowerasing "${PACKAGES_KERNEL_SURFACE[@]}"
+
+# Remove the default Fedora kernel and related packages
+dnf5 -y remove "${PACKAGES_KERNEL_DEFAULT[@]}"
+
+# Rebuild initramfs
+KERNEL_QUALIFIED="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+/usr/bin/dracut --no-hostonly --kver "${KERNEL_QUALIFIED}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_QUALIFIED}/initramfs.img"
+chmod 0600 "/lib/modules/${KERNEL_QUALIFIED}/initramfs.img"
+
+# Prevent kernel from upgrading again
+dnf5 versionlock add ${KERNEL_LOCK}
 
 dnf5 clean all
